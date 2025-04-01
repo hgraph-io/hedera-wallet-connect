370f5d3d369d7504dd0a3444db389e2d2b01f677
Tyler McDonald <tyler@hgraph.io>
2025-04-01 11:18:05 -0600

    browser events

--- src/reown/providers/EIP155Provider.ts
+++ src/reown/providers/EIP155Provider.ts
@@ -1,4 +1,4 @@
-import { EventEmitter } from 'node:events'
+import { EventEmitter } from 'events'
 // import Client from '@walletconnect/sign-client'
 import Client from '@walletconnect/universal-provider/node_modules/@walletconnect/sign-client'
 import { JsonRpcProvider } from '@walletconnect/jsonrpc-provider'
